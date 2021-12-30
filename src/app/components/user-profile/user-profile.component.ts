db769f90488abebc5e6ef5485f94ac7e969514b7
David Sule <david.sule@outlook.com>
2021-12-30 10:10:27 +0300

    Changed currentUser to type 'any'

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { User } from 'src/app/shared/user';
 import { AuthService } from './../../shared/auth.service';
 
@@ -9,25 +9,26 @@ import { AuthService } from './../../shared/auth.service';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
-  //currentUser: Object = {};
-  currentUser: Object = {
-    _id: undefined,
-    name: undefined,
-    email: undefined,
-    password: undefined
-  };
+  currentUser: any = {};
+  //currentUser: User[] = [];
 
   constructor(
     public authService: AuthService,
     private actRoute: ActivatedRoute
   ) { 
-    let id = this.actRoute.snapshot.paramMap.get('id');
+    /*let id = this.actRoute.snapshot.paramMap.get('id');
     this.authService.getUserProfile(id).subscribe(res => {
       this.currentUser = res.msg;
-    })
+    })*/
   }
 
   ngOnInit(): void {
+    this.getUserProfile();
+  }
+
+  getUserProfile(): void {
+    const id = parseInt(this.actRoute.snapshot.paramMap.get('id')!, 10);
+    this.authService.getUserProfile(id).subscribe(res => this.currentUser = res.data)
   }
 
 }
